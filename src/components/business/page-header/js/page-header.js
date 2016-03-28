06c0da40c477a120b007e6d4a337e524d97ea5e4
lux <lf790960502@163.com>
2016-03-28 09:01:15 +0800

    temp

---
+++ src/components/business/page-header/js/page-header.js
@@ -0,0 +1,13 @@
+
+
+require('../css/page-header.css');
+
+var React = require('react');
+
+var pageHeader = React.createClass({
+	render: function(){
+		return <h1>hello React</h1>;
+	}
+});
+
+module.exports = pageHeader;
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	entry: {'./page/index/js/index.js'},
+	output: {
+		
+	}
+};
